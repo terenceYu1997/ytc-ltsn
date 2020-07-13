c10fc9102fe1f14e2d6e33ca9ace526d8722c728
terenceYu1997 <terence.yu1997@gmail.com>
2020-07-14 00:08:52 +0800

    feat: '完成npm link测试'

---
+++ READEME.md
@@ -0,0 +1,4 @@
+### Useage
+
+1. `npm install ytc-ltsn -g`
+2. `ytc-ltsn 10`
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Terence on 2020/7/12 - 11:53 下午
+ * Description :
+ */
+const query = require('./lib/query')
+const update = require('./lib/update')
+module.exports = {
+  query,
+  update
+}
